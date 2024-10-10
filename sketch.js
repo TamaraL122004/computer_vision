13067cea7f52af794d500411966e96e6dcd95e48
TamaraL122004 <tamara.lamas@ucuenca.edu.ec>
2024-10-09 23:03:13 -0500

    4

--- sketch.js
+++ sketch.js
@@ -1,66 +1,3 @@
-// // Classifier Variable de imagenes, no asigna
-// let classifier;
-// // Model URL
-// let imageModelURL = "https://teachablemachine.withgoogle.com/models/jsHnyYQFn/";
-
-// // Video variable
-// let video;
-// let flippedVideo;
-// // To store the classification, almacena las etiquetas, ejm: crema, espejo
-// let label = "";
-
-// // Load the model first, se debe ejecutar antes del set up
-// function preload() {
-//   classifier = ml5.imageClassifier(imageModelURL + "model.json");
-//   //es la variable que se puso anteriormente, si se pone el link mas el model.json, nos sale el codigo para interpretar imagenes
-// }
-
-// function setup() {
-//   createCanvas(320, 260);
-//   // Create the video, enciende la cámara, funcion que se puso anteriormente
-//   video = createCapture(VIDEO);
-//   video.size(320, 240);
-//   video.hide();
-
-//   //flippedVideo = ml5.flipImage(video);
-//   // Start classifying
-//   classifyVideo();
-// }
-
-// function draw() {
-//   background(0);
-//   // Draw the video
-//   image(video, 0, 0);
-
-//   // Draw the label
-//   fill(255);
-//   textSize(16);
-//   textAlign(CENTER);
-//   text("hola", width / 2, height - 4);
-// }
-
-// // Get a prediction for the current video frame
-// function classifyVideo() {
-//   classifier.classify(video, gotResult); //la funcion puede tener el nombre que quiera
-// }
-
-// // When we get a result
-// function gotResult(results, error) {
-//   // If there is an error
-//   if (error) {
-//     console.error(error);
-//     return;
-//   }
-//   console.log(results);
-//   return;
-//   // The results are in an array ordered by confidence.
-//   // console.log(results[0]);
-//   label = results[0].label;
-
-//   // Classifiy again!
-//   classifyVideo();
-// }
-
 // Classifier Variable
 let classifier;
 // Model URL
@@ -102,10 +39,10 @@ function draw() {
   image(video, 0, 0);
 
   // Draw the label
-  fill(255);
-  textSize(16);
-  textAlign(CENTER);
-  text(label, width / 2, height - 4);
+  // fill(255);
+  // textSize(16);
+  // textAlign(CENTER);
+  // text(label, width / 2, height - 4);
 
   if (label == "delfin" && confiaza > 0.8) {
     // filter(OPAQUE);
@@ -137,7 +74,7 @@ function classifyVideo() {
 }
 
 // When we get a result
-function gotResult(results, error) {
+function gotResult(error, results) {
   // If there is an error
   if (error) {
     console.error(error);
@@ -146,6 +83,7 @@ function gotResult(results, error) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
+  confiaza = results[0].confidence;
   // Classifiy again!
   classifyVideo();
 }
